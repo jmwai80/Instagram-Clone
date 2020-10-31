b1ed65d904f16771b1a9fb866d01d1c0220161db
John Mwai <jmwai@conncol.edu>
2020-10-31 17:34:55 -0400

    Parse

--- app/src/main/java/com/example/insta/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/insta/ui/home/HomeFragment.java
@@ -1,35 +1,72 @@
 package com.example.insta.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.insta.Post;
 import com.example.insta.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
-public class HomeFragment extends Fragment {
+import java.io.File;
+import java.util.List;
 
+public class HomeFragment extends Fragment
+{
+    public static final String TAG = "PostsFragment";
+
+    private RecyclerView rvPosts;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.homepage);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        view.findViewById(R.id.rvPosts);
+
+        rvPosts = view.findViewById(R.id.rvPosts);
+        queryPosts();
+    }
+
+    private void queryPosts()
+    {
+        ParseQuery<Post> postQuery = new ParseQuery<>(Post.class);
+
+        postQuery.include(Post.KEY_USER);
+        postQuery.findInBackground(new FindCallback<Post>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void done(List<Post> objects, ParseException e) {
+                if(e != null)
+                {
+                    Log.e(TAG, "Error with query");
+                    e.printStackTrace();
+                    return;
+                }
+
+                for(int i = 0; i < objects.size(); i++)
+                {
+                    Post post = objects.get(i);
+                }
             }
         });
-        return root;
     }
 }
